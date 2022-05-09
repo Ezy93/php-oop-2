1a180a96a74a518093514d4c42be569c1d519529
Ezy93 <giordani001@gmail.com>
2022-05-09 14:58:02 +0200

    creata classe user ed aggiunte le proprietà

---
+++ classes/user.php
@@ -0,0 +1,37 @@
+<?php  
+    class User {
+        private $userName;
+        protected $password;
+        private $isRegistered;
+        private $hostID;
+        private $isEmployee;
+        private $address;
+        private $email;
+        private $telephoneNumber;
+        
+        public function __construct($userName = "" ,$password = "" ,$isEmployee=false ,$address ,$email ,$telephoneNumber)
+        {
+            $this->userName = $userName;
+            $this->password = $password;
+            if ($userName && $password == null) {
+                $this->isREgistered = false;
+            }else{
+                $this->isRegistered = true;
+            }
+
+            if (!$this->isRegistered) {
+                $this->hostID = rand(1000,10000000);
+            }else{
+                $this->hostID = null;
+            }
+
+            $this->isEmployee = $isEmployee;
+            $this->address = $address;
+            $this->email = $email;
+            $this->telephoneNumber = $telephoneNumber;
+        }
+
+        
+    }
+    
+?>
