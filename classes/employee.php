58adccdd6ee48c426dd59ca64cf5cf2b43684faf
Ezy93 <giordani001@gmail.com>
2022-05-10 15:25:53 +0200

    aggiunti metodi setter ad employee

--- classes/employee.php
+++ classes/employee.php
@@ -5,23 +5,88 @@
         private $employeeID;
         private $level;
         private $role;
-
-        function __construct($employeeID ,$level ,$role ,$name ,$surname ,$userName = null ,$password = null ,$isEmployee=false ,$address ,$email ,$telephoneNumber ,$creditCardNo)
+        
+        /**
+         * __construct
+         *
+         * @param  int $employeeID
+         * @param  int $level
+         * @param  string $role
+         * @param  string $name
+         * @param  string $surname
+         * @param  string $userName
+         * @param  string $password
+         * @param  bool $isEmployee
+         * @param  string $address
+         * @param  string $email
+         * @param  string $telephoneNumber
+         * @return void
+         */
+        function __construct($employeeID ,$level ,$role ,$name ,$surname ,$userName = null ,$password = null ,$isEmployee=false ,$address ,$email ,$telephoneNumber)
         {
             $this->employeeID = $employeeID;
             $this->level = $level;
             $this->role = $role;
-            parent::__construct($name ,$surname ,$userName = null ,$password = null ,$isEmployee=false ,$address ,$email ,$telephoneNumber ,$creditCardNo );
+            parent::__construct($name ,$surname ,$userName = null ,$password = null ,$isEmployee=false ,$address ,$email ,$telephoneNumber);
         }
 
+        /* METODI SETTER */     
+        
+        /**
+         * setEmployeeID
+         *
+         * @param  int $employeeID
+         * @return void
+         */
+        public function setEmployeeID($employeeID){
+            $this->employeeID = $employeeID;
+        }
+        
+        /**
+         * setLevel
+         *
+         * @param  int $level
+         * @return void
+         */
+        public function setLevel($level){
+            $this->level = $level;
+        }
+        
+        /**
+         * setRole
+         *
+         * @param  string $role
+         * @return void
+         */
+        public function setRole($role){
+            $this->role = $role;
+        }
+        
+        
+        /* METODI GETTER */        
+        /**
+         * getEmplyeedID
+         *
+         * @return int
+         */
         public function getEmplyeedID(){
             return $this->employeeID;
         }
-
+        
+        /**
+         * getLevel
+         *
+         * @return int
+         */
         public function getLevel(){
             return $this->level;
         }
-
+        
+        /**
+         * getRole
+         *
+         * @return string
+         */
         public function getRole(){
             return $this->role;
         }
